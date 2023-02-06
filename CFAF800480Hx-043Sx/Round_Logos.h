938a3e79c39d4e9a5378324aae8c59fe634e4a52
khayamxs <117682797+khayamxs@users.noreply.github.com>
2023-02-06 11:20:57 -0800

    Initial commit

---
+++ CFAF800480Hx-043Sx/Round_Logos.h
@@ -0,0 +1,24 @@
+#ifndef __ROUND_LOGOS_H__
+#define __ROUND_LOGOS_H__
+
+#if 1==LOGO_DEMO
+//===========================================================================
+#if 1==LOGO_PNG_0_ARGB2_1
+
+#define LOGO_SIZE_ARGB2  (3656)
+#define LOGO_WIDTH_ARGB2  (340)
+#define LOGO_HEIGHT_ARGB2 (340)
+extern const uint8_t CFAF800480Hx_043Sx_ARGB2_LOGO[LOGO_SIZE_ARGB2] PROGMEM;
+
+#endif // 1==LOGO_PNG_0_ARGB2_1
+//===========================================================================
+#if 0==LOGO_PNG_0_ARGB2_1
+
+#define LOGO_SIZE_PNG  (8154)
+extern const uint8_t CFAF800480Hx_043Sx_PNG_LOGO[LOGO_SIZE_PNG] PROGMEM;
+
+#endif // 0==LOGO_PNG_0_ARGB2_1
+//===========================================================================
+#endif // 1==LOGO_DEMO
+
+#endif // __ROUND_LOGOS_H__
---
+++ README.md
@@ -0,0 +1,2 @@
+# CFAF800480Hx-043Sx
+ Demo code for the CFAF800480Hx-043Sx family of displays
